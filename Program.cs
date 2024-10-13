4a7d0140570e75ae933d978edcddf2260cd950a5
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2024-10-13 01:37:21 -0400

    Modified message.
    Clarified end message

--- Program.cs
+++ Program.cs
@@ -4,4 +4,4 @@ Console.WriteLine("Hello, World!".Color("#2FAEB4").Bold());
 for(int x = 1; x <= 15; x++) {
     Console.WriteLine($"X = {x}");
 }
-Console.WriteLine("Done!".Color("#2390CF").Italic());
+Console.WriteLine("Done with loop!".Color("#2390CF").Italic());
