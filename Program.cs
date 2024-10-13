54b7e5aa5e8f42e164b0dd54debe564259ccc287
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2024-10-13 01:30:46 -0400

    Initial commit
    Begin work

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Color;
+Console.WriteLine("Hello, World!".Color("#2FAEB4").Bold());
+for(int x = 1; x <= 15; x++) {
+    Console.WriteLine($"X = {x}");
+}
+Console.WriteLine("Done!".Color("#2390CF").Italic());
