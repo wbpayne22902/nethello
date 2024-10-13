865efed22f84559f943eafa06a364a7bebbff1db
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2024-10-13 01:58:57 -0400

    Added version comment
    Added a version number to the source.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// Version 1.0
 using Color;
 Console.WriteLine("Hello, World!".Color("#2FAEB4").Bold());
 for(int x = 1; x <= 15; x++) {
